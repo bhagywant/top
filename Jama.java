d3157b3aba5405d15e2ddb4c8d354f36a7e952d9
Pawan Sadatkar <p.sadatka@direction.biz>
2021-05-07 10:13:45 +0530

    files commited

---
+++ Jama.java
@@ -0,0 +1,5 @@
+public class Jama{
+public static void main (String [] args){
+
+}
+}
--- Manu.java
+++ Manu.java
@@ -1,5 +1,5 @@
 public class Manu{
 public static void main(String [] args){
-System.out.println("Hi");
+System.out.println("Hi DF");
 }
 }
